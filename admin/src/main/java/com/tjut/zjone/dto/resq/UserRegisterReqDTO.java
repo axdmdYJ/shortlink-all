3c7487e26c35e387cdca763503533757a99e0be2
axdmdyj <2991142800@qq.com>
2024-01-22 20:13:54 +0800

    feature： 开发短链接后管系统对象自动填充

---
+++ admin/src/main/java/com/tjut/zjone/dto/resq/UserRegisterReqDTO.java
@@ -0,0 +1,42 @@
+package com.tjut.zjone.dto.resq;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
+
+@Data
+public class UserRegisterReqDTO {
+    /**
+     * ID
+     */
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 真实姓名
+     */
+    private String realName;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String mail;
+
+
+
+}
