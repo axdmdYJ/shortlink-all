f5e3a4cea89c9014bbd551a82bb507003d36dffd
axdmdyj <2991142800@qq.com>
2024-02-07 19:51:40 +0800

    feature：新增统计今日持久层

---
+++ project/src/main/java/com/tjut/zjone/service/LinkStatsTodayMapper.java
@@ -0,0 +1,10 @@
+package com.tjut.zjone.service;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.tjut.zjone.dao.entity.LinkStatsTodayDO;
+
+/**
+ * 统计今日持久层
+ */
+public interface LinkStatsTodayMapper extends BaseMapper<LinkStatsTodayDO> {
+}
